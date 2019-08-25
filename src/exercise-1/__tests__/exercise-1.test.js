3fdc79a1faa485793b034ecf424b7c20dcf79044
kelylah <kelylah@wp.pl>
2019-08-25 16:44:21 +0200

    [ex1] Moje ćwiczenia z części 1

--- src/exercise-1/__tests__/exercise-1.test.js
+++ src/exercise-1/__tests__/exercise-1.test.js
@@ -1,3 +1,13 @@
-test('print statement in text', () => {
-  expect(0).toBe(0);
+import invoices from '../invoices.json';
+import vehicles from '../vehicles.json';
+import {statement, renderStatementToHtml} from '../exercise-1.js';
+
+test('print statement in text', () =>{
+  const result = statement(invoices[0], vehicles);
+  expect(result).toMatchSnapshot();
+});
+
+test('print statement in html', () =>{
+  const result = renderStatementToHtml(invoices[0], vehicles);
+  expect(result).toMatchSnapshot();
 });
--- src/exercise-1/exercise-1.js
+++ src/exercise-1/exercise-1.js
@@ -1,34 +1,118 @@
-export default function app(invoice, vehicles) {
-  let totalAmount = 0;
-  let volumeCredits = 0;
-  let result = `Statement for ${invoice.customer}\n`;
-  const format = new Intl.NumberFormat('pl-PL', { style: 'currency', currency: 'PLN', minimumFractionDigits: 2, }).format;
-  for (let ride of invoice.rides) { const vehicle = vehicles[ride.vehicleId];
-    let thisAmount = 0;
-    switch (vehicle.type) {
-      case 'eco':        thisAmount = 500;
-        if (ride.minutes > 30) {
-          thisAmount += 20 * (ride.minutes - 30);
-        }
-        break;
-      case 'electric':        thisAmount = 1000;
-        if (ride.minutes > 10) {
-          thisAmount += 500 + 150 * (ride.minutes - 10);
-        }        thisAmount += 50 * ride.minutes;
-        break;
-      default:
-        throw new Error(`unknown type: ${vehicle.type}`);
-    }
-    // add volume credits
-    volumeCredits += Math.max(ride.minutes - 30, 0);
-    // add extra credit for every ten eco minutes
-    if ('eco' === vehicle.type) volumeCredits += Math.floor(ride.minutes / 5);
-    // print line for this ride
-    result += ` ${vehicle.name}: ${format(thisAmount / 100)} (${
-      ride.minutes    } minutes)\n`;
-    totalAmount += thisAmount;
+export function statement(invoice, vehicles) {
+  const statementData = createStatementData(invoice, vehicles);
+  return renderTxtStatement(statementData);
+}
+
+export function renderStatementToHtml(invoice, vehicles) {
+  const statementData = createStatementData(invoice, vehicles);
+
+  return renderHtml(statementData);
+}
+
+function renderHtml(data) {
+  let result = `<h1>Statement for ${data.customer}</h1>\n`;
+  result += '<table>\n';
+  result += '<tr><th>vehicle</th><th>minutes</th><th>cost</th></tr>\n';
+  for (let ride of data.rides) {
+    result += `<tr><td>${ride.vehicle.name}</td><td>${ride.minutes}</td>`;
+    result += `<td>${formatAsPln(ride.amount)}</td></tr>\n`;
   }
-  result += `Amount owed is ${format(totalAmount / 100)}\n`;
-  result += `You earned ${volumeCredits} credits\n`;
+  result += '</table>\n';
+  result += `<p>Amount owed is <em>${formatAsPln(data.totalAmount)}</em></p>\n`;
+  result += `<p>You earned <em>${data.volumeCredits}</em> credits</p>\n`;
   return result;
 }
+
+function renderTxtStatement(data) {
+  let result = `Statement for ${data.customer}\n`;
+  for (let ride of data.rides) {
+    // print line for this ride
+    result += ` ${ride.vehicle.name}: ${formatAsPln(ride.amount)} (${ride.minutes} minutes)\n`;
+  }
+  result += `Amount owed is ${formatAsPln(data.totalAmount)}\n`;
+  result += `You earned ${data.volumeCredits} credits\n`;
+  return result;
+
+}
+
+function formatAsPln(number) {
+  return new Intl.NumberFormat('pl-PL', {
+    style: 'currency',
+    currency: 'PLN',
+    minimumFractionDigits: 2,
+  }).format(number / 100);
+}
+
+function createStatementData(invoice, vehicles) {
+  const result = {};
+  result.customer = invoice.customer;
+  result.rides = invoice.rides.map(extendRides);
+  result.totalAmount = totalAmount(result);
+  result.volumeCredits = totalVolumeCredits(result);
+  return result;
+
+  function extendRides(ride) {
+    const result = Object.assign({}, ride);
+    result.vehicle = vehicleFor(ride);
+    result.amount = amountFor(result);
+    return result;
+  }
+
+  function totalVolumeCredits(data) {
+    let result = 0;
+    for (let ride of data.rides) {
+      result += volumeCreditsFor(ride);
+    }
+    return result;
+  }
+
+  function volumeCreditsFor(ride) {
+    let result = 0;
+    result += Math.max(ride.minutes - 30, 0);
+    // add extra credit for every ten eco minutes
+    if ('eco' === ride.vehicle.type)
+      result += Math.floor(ride.minutes / 5);
+    return result;
+  }
+
+  function totalAmount(data) {
+    let result = 0;
+    for (let ride of data.rides) {
+      result += ride.amount;
+    }
+    return result;
+  }
+
+  //To w tym miejscu moge sprawdzic, czy nie przyszlo cos pustego
+  function vehicleFor(ride) {
+    return vehicles[ride.vehicleId];
+  }
+
+  function amountFor(ride) {
+    let result = 0;
+    switch (ride.vehicle.type) {
+      case 'eco':
+        result = 500;
+        if (ride.minutes > 30) {
+          result += 20 * (ride.minutes - 30);
+        }
+        break;
+      case 'electric':
+        result = 1000;
+        if (ride.minutes > 10) {
+          result += 500 + 150 * (ride.minutes - 10);
+        }
+        result += 50 * ride.minutes;
+        break;
+      default:
+        throw new Error(`unknown type: ${ride.vehicle.type}`);
+    }
+    return result;
+  }
+}
+
+
+
+
+
+
